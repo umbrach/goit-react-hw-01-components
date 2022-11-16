f141d9b89c705ce12e152781d49fd513576f2f39
Denys Barabanov <umbrach415@gmail.com>
2022-11-17 00:43:23 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
+import Profile from './Profile/Profile';
+import user from '../json/user.json';
+import Statistics from './Statistics/Statistics';
+import data from '../json/data.json';
+import FriendList from './FriendsList/FriendList';
+import friends from '../json/friends.json';
+import TransactionHistory from './Transaction/TransactionHistory';
+import transactions from '../json/transactions.json';
+
+export default function App() {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />;
     </div>
   );
-};
+}
